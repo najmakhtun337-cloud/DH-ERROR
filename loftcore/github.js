2e9427fa2fab9272168564f2f2017536762635a6
xmdloft23 <xmdloft745@gmail.com>
2025-12-03 13:19:24 +0300

    Update github.js

--- loftcore/github.js
+++ loftcore/github.js
@@ -10,7 +10,7 @@ async function githubCommand(sock, chatId, message) {
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
-    let txt = `*乂  ʟᴏꜰᴛ Qᴜᴀɴᴛᴜᴍ  乂*\n\n`;
+    let txt = `*⚡  ʟᴏꜰᴛ Qᴜᴀɴᴛᴜᴍ  ⚡*\n\n`;
     txt += `✩  *Name* : ${json.name}\n`;
     txt += `✩  *Watchers* : ${json.watchers_count}\n`;
     txt += `✩  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
